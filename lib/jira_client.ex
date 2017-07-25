900aa0345ddca79f32d1ad461f8bd5bd02268608
Zack Kayser <zkayser@C02TT3W0HTD8.local>
2017-07-24 22:03:09 -0400

    Initial commit

---
+++ lib/jira_client.ex
@@ -0,0 +1,18 @@
+defmodule JiraClient do
+  @moduledoc """
+  Documentation for JiraClient.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JiraClient.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jira_client_test.exs
@@ -0,0 +1,8 @@
+defmodule JiraClientTest do
+  use ExUnit.Case
+  doctest JiraClient
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
