ebe68f93943a26388e842dcf1e2cae10d5fccac5
Zack Kayser <zkayser@C02TT3W0HTD8.local>
2017-07-31 00:28:16 -0400

    Eliminate test-only file storage

--- lib/jira_client/fileutils.ex
+++ lib/jira_client/fileutils.ex
@@ -1,60 +1,27 @@
 defmodule JiraClient.FileUtils do
   @creds_file "credentials.txt"
   @creds_dir ".jira"
-  @test_creds_dir ".jira_test"
 
-  def mkdir_for_credentials(env \\ :dev) do
-    case env do
-      :dev -> mkdir(@creds_dir)
-      :test -> mkdir(@test_creds_dir)
-    end
-  end
-
-  defp mkdir(dir_name) do
-    unless Path.expand("~") |> Path.join(dir_name) |> File.exists?() do
+  def mkdir_for_credentials do
+    unless Path.expand("~") |> Path.join(@creds_dir) |> File.exists?() do
       Path.expand("~") |> File.cd()
-      File.mkdir(dir_name)
+      File.mkdir(@creds_dir)
     end
   end
 
-  def write_credentials(env \\ :dev, credentials) do
-    case env do
-      :dev -> do_write_credentials(@creds_dir, credentials)
-      :test -> do_write_credentials(@test_creds_dir, credentials)
-    end
-  end
-
-  defp do_write_credentials(dir_name, credentials) do
+  def write_credentials(credentials) do
     Path.expand("~")
-    |> Path.join(dir_name)
+    |> Path.join(@creds_dir)
     |> File.cd()
     File.write(@creds_file, credentials)
   end
 
-  def creds_file_exists?(env \\ :dev) do
-    case env do
-      :dev -> check_file_exists("#{@creds_dir}/#{@creds_file}")
-      :test -> check_file_exists("#{@test_creds_dir}/#{@creds_file}")
-    end
-  end
+  def creds_file_exists?, do: Path.expand("~") |> Path.join("#{@creds_dir}/#{@creds_file}") |> File.exists?()
 
-  defp check_file_exists(file), do: Path.expand("~") |> Path.join(file) |> File.exists?()
+  def get_creds_file, do: Path.expand("~") |> Path.join("#{@creds_dir}/#{@creds_file}")
 
-  def get_creds_file(env \\ :dev) do
-    case env do
-      :dev -> Path.expand("~") |> Path.join("#{@creds_dir}/#{@creds_file}")
-      :test -> Path.expand("~") |> Path.join("#{@test_creds_dir}/#{@creds_file}")
-    end
-  end
-
-  def delete_creds_file(env \\ :dev) do
-    case env do
-      :dev -> File.rm_rf(get_creds_file())
-      :test -> File.rm_rf(get_creds_file(:test))
-    end
-  end
+  def delete_creds_file, do: File.rm_rf(get_creds_file())
 
   def creds_file, do: @creds_file
-  def creds_dir(:dev), do: @creds_dir
-  def creds_dir(:test), do: @test_creds_dir
+  def creds_dir, do: @creds_dir
 end
