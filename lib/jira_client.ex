f8bd4ff1998ca7efce69efc25aea9e14e9f5dcac
Edward Sumerfield <esumerfield@C02Q7KGZG8WP.local>
2017-08-02 10:50:11 -0400

    add test for output generated by main method

--- lib/jira_client.ex
+++ lib/jira_client.ex
@@ -2,7 +2,7 @@ defmodule JiraClient do
   alias JiraClient.Args
 
   def main(args) do
-    IO.puts("Welcome to Jirs Client: #{args}")
+    IO.puts("Welcome to Jira Client: #{args}")
 
     IO.inspect Args.parse(args)
   end
--- test/jira_client_test.exs
+++ test/jira_client_test.exs
@@ -2,7 +2,13 @@ defmodule JiraClientTest do
   use ExUnit.Case
   doctest JiraClient
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import ExUnit.CaptureIO
+
+  test "call main" do
+    output = capture_io(fn ->
+      JiraClient.main(["invalid"])
+    end)
+
+    assert Regex.match?(~r/Welcome to Jira Client:.*/, output), "Output doesn't match expected text: #{output}"
   end
 end
