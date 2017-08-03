f10e99eadaeadb530a00bc8ae352bb83d59e1530
Edward Sumerfield <esumerfield@C02Q7KGZG8WP.local>
2017-08-03 11:13:33 -0400

    start flushing out close issue and command run semantics

--- lib/jira_client/command/close_issue.ex
+++ lib/jira_client/command/close_issue.ex
@@ -1,5 +1,8 @@
 defmodule JiraClient.Command.CloseIssue do
+
+  @spec run(JiraClient.Args.t) :: String.t
   def run(args) do
 
+    inspect args
   end
 end
--- test/jira_client/command/close_issue_test.exs
+++ test/jira_client/command/close_issue_test.exs
@@ -2,7 +2,9 @@ defmodule JiraClient.Command.CloseIssueTest do
   use ExUnit.Case
   doctest JiraClient.Command.CloseIssue
 
+  alias JiraClient.Args
+
   test "execution" do
-    JiraClient.Command.CloseIssue.run(%{})
+    JiraClient.Command.CloseIssue.run(Args.parse(["create_issue", "-p", "PROJECT A", "-f", "1.2.3", "-m", "MESSAGE 1"]))
   end
 end
