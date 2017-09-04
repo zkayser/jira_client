0728d570e6dad4614015e54095d14e48587b2578
Edward Sumerfield <esumerfield@C02Q7KGZG8WP.local>
2017-09-04 11:20:56 -0400

    refactor names of test_credentials to credentials_mock

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :jira_client, 
   base_url:         "https://vpsadm.ipcoop.com/jira",
-  test_credentials: JiraClient.Auth.Credentials,
+  credentials_module: JiraClient.Auth.Credentials,
   command_module:   JiraClient.Command,
   request_module:   JiraClient.Http.Request,
   file_module:      File
