a103899bbf149e1cade8541929327a098c5fc95d
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-09-24 14:54:40 -0400

    add support for tracking requests sent using the fake

--- test/jira_client/http/request_fake.ex
+++ test/jira_client/http/request_fake.ex
@@ -2,7 +2,7 @@ defmodule JiraClient.Http.RequestFake do
   alias JiraClient.Http.Request
 
   def init() do
-    with {:ok, _} <- Agent.start_link(fn -> [] end, name: __MODULE__)
+    with {:ok, _} <- Agent.start_link(fn -> %{requests: [], responses: []} end, name: __MODULE__)
     do
       :ok
     else
@@ -16,39 +16,61 @@ defmodule JiraClient.Http.RequestFake do
   end
 
   def clear() do
-    Agent.update(__MODULE__, fn _ -> [] end)
+    Agent.update(__MODULE__, fn _ -> %{requests: [], responses: []} end)
   end
 
   def expect_response(response) do
-    Agent.update(__MODULE__, fn responses -> 
-        responses ++ [response]
-      end)
+    Agent.update(__MODULE__, fn state -> 
+      %{requests: state.requests, responses: state.responses ++ [response]}
+    end)
   end
 
   # TODO this is a 2 message sequence to a single threaded process which could
   # result in a race condition if mutlople tests are running.
   def next_response() do
-    next_response = Agent.get(__MODULE__, fn responses -> 
-        case responses do
+    next_response = Agent.get(__MODULE__, fn state -> 
+        case state.responses do
           [next_response|_] -> next_response
           [] -> ""
         end
       end)
-    Agent.update(__MODULE__, fn responses -> 
-        case responses do
-          [_|responses] -> responses
-          [] -> ""
+    Agent.update(__MODULE__, fn state -> 
+        case state.responses do
+          [_|responses] -> %{requests: state.requests, responses: responses}
+          [] -> %{requests: state.requests, responses: []}
         end
       end)
 
     next_response
   end
 
+  # Get the requests that have been tracked
+  def next_request() do
+    next_request = Agent.get(__MODULE__, fn state -> 
+        case state.requests do
+          [next_request|_] -> {next_request.http_method, next_request.body, next_request.path}
+          [] -> {:none, "", ""}
+        end
+      end)
+    Agent.update(__MODULE__, fn state -> 
+        case state.requests do
+          [_|requests] -> %{requests: requests, responses: state.responses}
+          [] -> %{requests: [], responses: state.responses}
+        end
+      end)
+
+      next_request
+  end
+
   def new(method, body, path, creds_get_fn \\ fn -> "username:password" end) do
     Request.new(method, body, path, creds_get_fn)
   end
 
-  def send(_) do
+  def send(request) do
+    Agent.update(__MODULE__, fn state -> 
+      %{requests: state.requests ++ [request], responses: state.responses}
+    end)
+
     next_response()
   end
 end
--- test/jira_client/http/request_fake_test.exs
+++ test/jira_client/http/request_fake_test.exs
@@ -3,71 +3,110 @@ defmodule JiraClient.Http.RequestFakeTest do
 
   alias JiraClient.Http.RequestFake
 
-  test "start" do
-    assert :ok == RequestFake.init()
+  describe "managemnt" do
+
+    test "start" do
+      assert :ok == RequestFake.init()
+    end
+
+    test "start twice" do
+      assert :ok == RequestFake.init()
+      assert :ok == RequestFake.init()
+    end
+
+    test "default response" do
+      RequestFake.init()
+
+      assert "" == RequestFake.next_response()
+    end
   end
 
-  test "start twice" do
-    assert :ok == RequestFake.init()
-    assert :ok == RequestFake.init()
+  describe "store and retrieve responses" do
+
+    test "expect response" do
+      RequestFake.init()
+      RequestFake.expect_response("response1")
+
+      assert "response1" == RequestFake.next_response()
+    end
+    
+    test "expect a sequence of responses" do
+      RequestFake.init()
+      RequestFake.expect_response("response1")
+      RequestFake.expect_response("response2")
+
+      assert "response1" == RequestFake.next_response()
+      assert "response2" == RequestFake.next_response()
+      assert "" == RequestFake.next_response()
+    end
+
+    test "expect a longer  sequence of responses" do
+      RequestFake.init()
+      RequestFake.expect_response("response1")
+      RequestFake.expect_response("response2")
+      RequestFake.expect_response("response3")
+      RequestFake.expect_response("response4")
+      RequestFake.expect_response("response5")
+
+      assert "response1" == RequestFake.next_response()
+      assert "response2" == RequestFake.next_response()
+      assert "response3" == RequestFake.next_response()
+      assert "response4" == RequestFake.next_response()
+      assert "response5" == RequestFake.next_response()
+      assert "" == RequestFake.next_response()
+    end
+
+    test "clear expeted results" do
+      RequestFake.init()
+      RequestFake.expect_response("response1")
+
+      RequestFake.clear()
+
+      assert "" == RequestFake.next_response()
+    end
+
+    test "sequence polution ensure init clears" do
+      RequestFake.init()
+      RequestFake.expect_response("response1")
+
+      RequestFake.init()
+      RequestFake.expect_response("response2")
+
+      assert "response2" == RequestFake.next_response()
+    end
+
+    test "no expected response will return no response" do
+      RequestFake.init()
+
+      assert "" == RequestFake.next_response()
+    end
   end
 
-  test "default response" do
-    RequestFake.init()
+  describe "remember and return requests" do
+    test "capture and reutrn a request" do
+      RequestFake.init()
 
-    assert "" == RequestFake.next_response()
-  end
+      request = RequestFake.new(:get, "body", "path")
+      RequestFake.send(request)
 
-  test "expect response" do
-    RequestFake.init()
-    RequestFake.expect_response("response1")
+      assert {:get, "body", "path"} == RequestFake.next_request()
+    end
 
-    assert "response1" == RequestFake.next_response()
-  end
-  
-  test "expect a sequence of responses" do
-    RequestFake.init()
-    RequestFake.expect_response("response1")
-    RequestFake.expect_response("response2")
+    test "capture 2 requests and reutrn a request" do
+      RequestFake.init()
 
-    assert "response1" == RequestFake.next_response()
-    assert "response2" == RequestFake.next_response()
-    assert "" == RequestFake.next_response()
-  end
+      RequestFake.send(RequestFake.new(:get, "body", "path"))
+      RequestFake.send(RequestFake.new(:post, "{}", "/rest/api/projects"))
 
-  test "expect a longer  sequence of responses" do
-    RequestFake.init()
-    RequestFake.expect_response("response1")
-    RequestFake.expect_response("response2")
-    RequestFake.expect_response("response3")
-    RequestFake.expect_response("response4")
-    RequestFake.expect_response("response5")
+      assert {:get, "body", "path"} == RequestFake.next_request()
+      assert {:post, "{}", "/rest/api/projects"} == RequestFake.next_request()
+    end
 
-    assert "response1" == RequestFake.next_response()
-    assert "response2" == RequestFake.next_response()
-    assert "response3" == RequestFake.next_response()
-    assert "response4" == RequestFake.next_response()
-    assert "response5" == RequestFake.next_response()
-    assert "" == RequestFake.next_response()
-  end
+    test "get nothing when no requests have been sent" do
+      RequestFake.init()
 
-  test "clear expeted results" do
-    RequestFake.init()
-    RequestFake.expect_response("response1")
-
-    RequestFake.clear()
-
-    assert "" == RequestFake.next_response()
-  end
-
-  test "sequence polution ensure init clears" do
-    RequestFake.init()
-    RequestFake.expect_response("response1")
-
-    RequestFake.init()
-    RequestFake.expect_response("response2")
-
-    assert "response2" == RequestFake.next_response()
+      assert {:none, "", ""} == RequestFake.next_request()
+    end
   end
 end
 
