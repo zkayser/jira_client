115e22ce22520bf82fa5860a5b2ef3cc820c2aba
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-09-24 14:21:08 -0400

    correct the project versions request to use the correct http verb

--- lib/jira_client/api/project_versions/project_versions.ex
+++ lib/jira_client/api/project_versions/project_versions.ex
@@ -10,7 +10,7 @@ defmodule JiraClient.Api.ProjectVersions do
   # GET /rest/api/2/project/{projectIdOrKey}/versions
   @spec send(%{project_id: String.t}, String.t) :: {Atom.t, Sring.t}
   def send(attributes, body) do
-    response = @request.new(:post, body, "rest/api/latest/project/#{attributes.project_id}/versions")
+    response = @request.new(:get, body, "rest/api/latest/project/#{attributes.project_id}/versions")
     |> @request.send
 
     {:ok, response}
