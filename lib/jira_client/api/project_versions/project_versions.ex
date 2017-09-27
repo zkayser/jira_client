faa1fd6b5b2f8097567241c73601dac2c1233ead
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-09-26 20:51:56 -0400

    cleanup comment

--- lib/jira_client/api/project_versions/project_versions.ex
+++ lib/jira_client/api/project_versions/project_versions.ex
@@ -8,7 +8,9 @@ defmodule JiraClient.Api.ProjectVersions do
   @behaviour JiraClient.Api.Sender
 
   # GET /rest/api/2/project/{projectIdOrKey}/versions
-  @spec send(%{project_id: String.t}, String.t) :: {Atom.t, Sring.t}
+  # input:
+  #   attributes.project_id: String.t
+  #
   def send(attributes, _, logging \\ false) do
     response = @request.new(:get, "", "rest/api/latest/project/#{attributes.project_id}/versions")
     |> @request.send(logging)
