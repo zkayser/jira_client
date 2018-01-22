96e00e04fc13f5b74d00936e388410ba1f9d1ccb
Edward Sumerfield <edward.sumerfield@plxis.com>
2018-01-22 14:24:41 -0500

    correct alias generator delimiter

--- bin/project_aliases.sh
+++ bin/project_aliases.sh
@@ -37,8 +37,8 @@ jira_client list_projects | while read projectName; do
   done
   code=$uniqueCode
 
-  echo "alias tc${code}='track_change.sh --project \"${projectName}\" --message " >> $aliasFile
-  echo "alias jct${code}='jira_client create_issue --project \"${projectName}\" --message " >> $aliasFile
+  echo "alias tc${code}='track_change.sh --project \"${projectName}\" --message '" >> $aliasFile
+  echo "alias jct${code}='jira_client create_issue --project \"${projectName}\" --message '" >> $aliasFile
 
   foundCodes=$foundCodes,$code
 done
