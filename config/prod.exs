a4ca9b582aeac7e305c62f962b1d018eb1ab7cf2
Edward Sumerfield <esumerfield@stackct.com>
2019-03-03 17:38:12 -0500

    remove base_url configuration

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,6 @@
 use Mix.Config
 
 config :jira_client, 
-  base_url:           "https://plxistools.atlassian.net",
   credentials_module: JiraClient.Auth.Credentials,
   command_module:     JiraClient.Command,
   request_module:     JiraClient.Http.Request,
--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,6 @@ Code.require_file("lib/jira_client/http/request.ex")
 Code.require_file("test/jira_client/http/request_fake.ex")
 
 config :jira_client, 
-  base_url:           "http://intentionally_left_blank",
   credentials_module: JiraClient.Auth.CredentialsMock,
   command_module:     JiraClientTest.CommandFake,
   request_module:     JiraClient.Http.RequestFake,
