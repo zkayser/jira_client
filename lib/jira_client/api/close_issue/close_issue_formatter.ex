70049741e38b096fd8fbba406260535f9de1960c
Edward Sumerfield <esumerfield@stackct.com>
2019-03-09 15:01:08 -0500

    correct comment formatting

--- lib/jira_client/api/close_issue/close_issue_formatter.ex
+++ lib/jira_client/api/close_issue/close_issue_formatter.ex
@@ -2,53 +2,53 @@ defmodule JiraClient.Api.CloseIssueFormatter do
   @moduledoc """
   Documentation from https://developer.atlassian.com/cloud/jira/platform/rest/#api-api-2-issue-issueIdOrKey-transitions-post
   {
-    "update": {
-      "comment": [
-        {
-          "add": {
-              "body": "Bug has been fixed."
+      "update": {
+        "comment": [
+          {
+            "add": {
+                "body": "Bug has been fixed."
+            }
           }
-        }
-      ]
-    },
-    "fields": {
-      "assignee": {
-        "name": "bob"
+        ]
       },
-      "resolution": {
-        "name": "Fixed"
-      }
-    },
-    "transition": {
-        "id": "5"
-    },
-    "historyMetadata": {
-        "type": "myplugin:type",
-        "description": "text description",
-        "descriptionKey": "plugin.changereason.i18.key",
-        "activityDescription": "text description",
-        "activityDescriptionKey": "plugin.activity.i18.key",
-        "actor": {
-            "id": "tony",
-            "displayName": "Tony",
-            "type": "mysystem-user",
-            "avatarUrl": "http://mysystem/avatar/tony.jpg",
-            "url": "http://mysystem/users/tony"
+      "fields": {
+        "assignee": {
+          "name": "bob"
         },
-        "generator": {
-            "id": "mysystem-1",
-            "type": "mysystem-application"
-        },
-        "cause": {
-            "id": "myevent",
-            "type": "mysystem-event"
-        },
-        "extraData": {
-            "keyvalue": "extra data",
-            "goes": "here"
+        "resolution": {
+          "name": "Fixed"
         }
-    }
-}
+      },
+      "transition": {
+          "id": "5"
+      },
+      "historyMetadata": {
+          "type": "myplugin:type",
+          "description": "text description",
+          "descriptionKey": "plugin.changereason.i18.key",
+          "activityDescription": "text description",
+          "activityDescriptionKey": "plugin.activity.i18.key",
+          "actor": {
+              "id": "tony",
+              "displayName": "Tony",
+              "type": "mysystem-user",
+              "avatarUrl": "http://mysystem/avatar/tony.jpg",
+              "url": "http://mysystem/users/tony"
+          },
+          "generator": {
+              "id": "mysystem-1",
+              "type": "mysystem-application"
+          },
+          "cause": {
+              "id": "myevent",
+              "type": "mysystem-event"
+          },
+          "extraData": {
+              "keyvalue": "extra data",
+              "goes": "here"
+          }
+      }
+  }
   """
   @behaviour JiraClient.Api.Formatter
 
