da9f3ce36b8e294599d563ec30534a5519d343ad
Edward Sumerfield <esumerfield@stackct.com>
2019-03-10 17:33:10 -0400

    Add skeleton assign api call. Need a get user by username to get account id then an assign

---
+++ lib/jira_client/command/assign.ex
@@ -0,0 +1,44 @@
+defmodule JiraClient.Command.Assign do
+  @moduledoc """
+    An assign command assigns an issue to a user. If the user is not specified then the
+    username of the logged in user is assumed as the assign to me feature.
+
+    Documentation: https://developer.atlassian.com/cloud/jira/platform/rest/v3/#api-rest-api-3-issue-issueIdOrKey-assignee-put
+
+    curl --request PUT \
+      --url '/rest/api/3/issue/{issueIdOrKey}/assignee' \
+      --header 'Authorization: Bearer <access_token>' \
+      --header 'Accept: application/json' \
+      --header 'Content-Type: application/json' \
+      --data '{
+      "accountId": "384093:32b4d9w0-f6a5-3535-11a3-9c8c88d10192"
+    }'
+  """
+
+  #alias JiraClient.Api.Assign,          as: ApiAssign
+  #alias JiraClient.Api.AssignFormatter, as: ApiAssignFormatter
+  #alias JiraClient.Api.AssignParser,    as: ApiAssignParser
+
+  #@behaviour JiraClient.Command
+
+  #def run(args) do
+    #with {:ok, result}     <- assign_issue(args.issue, args.logging)
+    #do
+      #{:ok, "#{inspect result}"}
+    #else
+      #message -> message
+    #end
+  #end
+
+  #defp assign_issue(issue_id, logging) do
+    #with {:ok, request}  <- ApiAssignFormatter.format(%{issue_id: issue_id}),
+         #{:ok, response} <- ApiAssign.send(%{account_id: account_id}, request, logging),
+         #{:ok, _result}  <- ApiAssignParser.parse(response)
+    #do
+      #{:ok, "Assigned"}
+    #else
+      #message -> {:error, message}
+    #end
+  #end
+end
+
